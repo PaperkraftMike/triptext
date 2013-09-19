9355af0be4a3f65d7ba53343c2e61c8ce2a0c97e
Michael Cahill <miccahill@gmail.com>
2013-09-18 21:50:29 -0400

    Fixed map canvas

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,6 @@ TextMe::Application.routes.draw do
   
   resources :destinations
 
-
-
-
-
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
