86dace42c911b22e181646cfa5a09396c3bb5312
Michael Cahill <miccahill@gmail.com>
2013-09-24 07:00:14 -0400

    moved rake into it's own method

--- app/models/message.rb
+++ app/models/message.rb
@@ -9,7 +9,26 @@ class Message < ActiveRecord::Base
     @number.save
   end
 
-  
+  def send_messages
+    Message.all.each do |t|
+      if t.confirmation.nil? && !t.dispatch_on.nil?
+        if t.dispatch_on - 5.minutes <= DateTime.now
+          require 'twilio-ruby'
+          twilio_sid = ENV['TWILIO_ACCOUNT_SID']
+          twilio_auth_token = ENV['TWILIO_AUTH_TOKEN']
+          client = Twilio::REST::Client.new(twilio_sid.to_s.strip, twilio_auth_token.to_s.strip)
+            client.account.sms.messages.create(
+              :from => ENV['TWILIO_NUMBER'],
+              :to => "#{Number.find(t.number).phone_number}",
+              :body => "I'm just around the corner. I'll be there soon."
+            )
+          message = Message.find(t.id)
+          message.confirmation = true
+          message.save
+          end
+        end
+      end
+    end
 
   has_one :number
   has_one :addresses
--- lib/tasks/messages.rake
+++ lib/tasks/messages.rake
@@ -1,26 +1,10 @@
 namespace :messages do
 
 task :check_messages => :environment do
-  Message.all.each do |t|
-      if t.confirmation.nil? && !t.dispatch_on.nil?
-        if t.dispatch_on - 5.minutes <= DateTime.now
-          require 'twilio-ruby'
-          twilio_sid = ENV['TWILIO_ACCOUNT_SID']
-          twilio_auth_token = ENV['TWILIO_AUTH_TOKEN']
-          client = Twilio::REST::Client.new(twilio_sid.to_s.strip, twilio_auth_token.to_s.strip)
-            client.account.sms.messages.create(
-              :from => ENV['TWILIO_NUMBER'],
-              :to => "#{Number.find(t.number).phone_number}",
-              :body => "I'm just around the corner. I'll be there soon."
-            )
-          message = Message.find(t.id)
-          message.confirmation = true
-          message.save
-          end
-        end
-      end
-    end
- end
+  Message.send_messages
+end
+
+end
 
 
 
