65582a2dd9bc18badcb319420e720e39d64d097c
Alexander Luce <alexluce@gmail.com>
2013-10-03 21:08:42 -0400

    Early mobile responsivness. Needs optimizing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
+  after_action :allow_iframe
+
   def index
   end
+
+private
+
+  def allow_iframe
+    response.headers['X-Frame-Options'] = 'ALLOW-FROM localhost'
+  end
+
 end 
