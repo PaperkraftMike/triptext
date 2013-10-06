a53fefa779cb356fe57d83c10acecebf444fa104
Michael Cahill <miccahill@gmail.com>
2013-10-06 14:35:31 -0400

    added about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ TextMe::Application.routes.draw do
 
   root :to => "home#index"
 
-
   resources :messages
 
   resources :locations
+
+  get '/about' => "pages#about"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
